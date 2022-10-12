ab79efb7f104ab1a066265492225ca2b9fa0dd2f
nokekip <kipnoke@gmail.com>
2022-10-12 12:44:54 -0700

    update

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    calc = 0
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception('Too far')
+            else:
+                calc += a ** b / i
+        except Exception:
+            calc = b + a
+            break
+    return calc
