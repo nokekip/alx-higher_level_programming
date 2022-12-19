2bec6777e87ba912c42313d662339ec0378631d2
nokekip <kipnoke@gmail.com>
2022-12-19 15:57:49 +0300

    update indentation

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 
-const line = 'C is fun\nPython is coon\nJavaScript is amazing';
-console.log(line);
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else if (process.argv.length <= 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
