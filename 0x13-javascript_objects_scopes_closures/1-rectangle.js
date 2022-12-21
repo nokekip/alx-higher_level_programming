eac89f9b36a3c20f51aca63c1e3adaf772ffcd3d
nokekip <kipnoke@gmail.com>
2022-12-21 17:44:09 +0300

    class rectangle with height and width attribute

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/** 
+ * represents a rectangle class with height and width.
+ */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
