626341f098a38a39e70af7cf9cac8876000057c5
nokekip <kipnoke@gmail.com>
2023-10-17 18:48:12 +0300

    creates table with id and nameyour

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -4,4 +4,4 @@
 --         id INT
 --         name VARCHAR(256)
 --     The database name will be passed as an argument of the mysql command
-CREATE TABLE IF EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
