2b653a7dfdab440b568c00d21d7e1a5201281379
nokekip <kipnoke@gmail.com>
2023-10-18 12:35:54 +0300

    edited passord

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- grants user_0d_1 all permission
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';git pull --no-rebase origin master
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
