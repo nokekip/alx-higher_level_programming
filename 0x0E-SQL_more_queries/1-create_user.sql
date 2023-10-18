68a8554708fb2aa84d6b34d27ab475649c66affa
nokekip <kipnoke@gmail.com>
2023-10-18 12:24:19 +0300

    create user and grant all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates mysql user user_0d_1 with password user_0d_1
+-- grants user_0d_1 all permission
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
