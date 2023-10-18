723fc69505f1bfd01b2ac485c9be0ca95745316d
nokekip <kipnoke@gmail.com>
2023-10-18 12:35:27 +0300

    edited passord

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates mysql user user_0d_1 with password user_0d_1
 -- grants user_0d_1 all permission
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';git pull --no-rebase origin master
