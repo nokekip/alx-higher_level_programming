f09fc9b3f6864496dc8f0043634e64f26f8aa056
nokekip <kipnoke@gmail.com>
2023-11-14 23:24:06 +0300

    check for argumenta

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/**
+ * checking for arguments in class rectangle
+ */
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
