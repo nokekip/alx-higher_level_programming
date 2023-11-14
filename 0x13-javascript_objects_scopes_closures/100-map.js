4cccdf758aba459ad3e28b3b3a2d2137e67a1b84
nokekip <kipnoke@gmail.com>
2023-11-15 02:07:31 +0300

    import list from the file 100-data.js and create new list

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/**
+ * import list from the file 100-data.js and create new list with each value equal to the value of the initial list, multipled by the index in the list
+ */
+
+const list = require('./100-data').list; // Import the list from 100-data.js
+const map1 = list.map((x, index) => x * index); // Create a new list with the values multiplied by their index
+console.log(list); // Print the original list
+console.log(map1); // Print the new list
