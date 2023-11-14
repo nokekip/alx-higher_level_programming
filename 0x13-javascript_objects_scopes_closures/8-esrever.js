1e581675db49f5617254d4dea5dd7084be9685d7
nokekip <kipnoke@gmail.com>
2023-11-15 01:37:05 +0300

    returns reversed list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/**
+ * reverses items in a list
+ */
+
+exports.esrever = function (list) {
+  const reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+};
