bedd218e5cf81c327d0bb123badfe813769e78e6
nokekip <kipnoke@gmail.com>
2023-11-14 23:22:55 +0300

    fix semi standard compliance

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -4,9 +4,9 @@
  * rectangle class with width and height attributes
  */
 class Rectangle {
-    constructor (w, h) {
-        this.width = w;
-        this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 module.exports = Rectangle;
