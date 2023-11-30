12318bcb78f06d19a02005d8c8fe120f84b4abd2
nokekip <kipnoke@gmail.com>
2023-12-01 00:16:16 +0300

    update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 # sends a POST request to the passed URL using curl, and display the body of the response
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
