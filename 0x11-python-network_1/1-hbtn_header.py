086c20b245d4096c13403188a2451e255d3dc7f5
nokekip <kipnoke@gmail.com>
2023-12-03 15:45:10 +0300

    fix pep8

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-""" Takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response """
+"""
+Takes in a URL, sends a request to the URL and displays the value of
+the X-Request-Id variable found in the header of the response
+"""
 
 import urllib.request
 import sys
