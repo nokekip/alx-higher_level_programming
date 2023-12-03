b7078ccfb3db5ea0f152621e1a281111f32ddabb
nokekip <kipnoke@gmail.com>
2023-12-03 13:53:31 +0300

    fix pep8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,11 @@
 
 import urllib.request
 
+
 def get_status():
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as url:
         return url.read()
+
 
 if __name__ == "__main__":
     # get status and print the body of the response
