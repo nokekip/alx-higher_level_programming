d6d17e6e32e5cafef94ee9172e9b11b44b67d05f
nokekip <kipnoke@gmail.com>
2023-12-03 18:18:10 +0300

    get X-Request-Id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Get X-Request-Id in the header of a url
+and how can i access it in code from the urllib package
+'''
+
+import requests
+import sys
+
+
+def get_x_request_id():
+    '''
+    function to get X-request-Id of a given url
+    '''
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-request-Id')
+    print(x_request_id)
+
+
+if __name__ == "__main__":
+    get_x_request_id()
