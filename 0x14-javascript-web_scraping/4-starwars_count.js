6be1d932d74482b1d48e9e215fbd2bede81bbd8f
nokekip <kipnoke@gmail.com>
2024-01-03 17:39:37 +0300

    refactor

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,14 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const characterId = 18;
 
 request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const filmsData = JSON.parse(body).results;
-    const moviesWithWedge = filmsData.filter((film) => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
-    console.log(moviesWithWedge.length);
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
