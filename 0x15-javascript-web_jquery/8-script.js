fbd55ee4ed0c4e284b28bf59a42342a14b2c5744
nokekip <kipnoke@gmail.com>
2024-01-10 19:47:51 +0300

    fetches and lists the title for all movies from star wars api

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function(data){
+        $.each(data.results, function(i, movie){
+            $('UL#list_movies').append('<li>' + movie.title + '</li>');
+        });
+    });
+});
